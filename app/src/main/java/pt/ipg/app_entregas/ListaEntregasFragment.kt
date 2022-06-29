69df3b719054fc49eeef2ffef20c584f3205fe45
afonsoalves12 <afonsosilvalves@gmail.com>
2022-06-29 15:34:53 +0100

    Alterado o nome do SecondFragment para ListaEntregasFragment

--- app/src/main/java/pt/ipg/app_entregas/ListaEntregasFragment.kt
+++ app/src/main/java/pt/ipg/app_entregas/ListaEntregasFragment.kt
@@ -6,14 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import pt.ipg.app_entregas.databinding.FragmentSecondBinding
+import pt.ipg.app_entregas.databinding.FragmentListaEntregasBinding
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class ListaEntregasFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentListaEntregasBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +25,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentListaEntregasBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,7 +34,7 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.action_ListaEntregas_to_MenuPrincipal)
         }
     }
 
