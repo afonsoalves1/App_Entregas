389b68973de4f214983864b66f9146298cfc42be
afonsoalves12 <afonsosilvalves@gmail.com>
2022-06-29 17:05:19 +0100

    Passar os dados dos livros para o Adapter(RecyclerView)

--- app/src/main/java/pt/ipg/app_entregas/AdapterEntregas.kt
+++ app/src/main/java/pt/ipg/app_entregas/AdapterEntregas.kt
@@ -1,10 +1,20 @@
 package pt.ipg.app_entregas
 
+import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class AdapterEntregas : RecyclerView.Adapter<AdapterEntregas.ViewHolderEntrega>()  {
+
+    var cursor: Cursor? = null
+        get() = field
+        set(value) {
+            if (field != value) {
+                field = value
+                notifyDataSetChanged()
+            }
+        }
 
     class ViewHolderEntrega(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
