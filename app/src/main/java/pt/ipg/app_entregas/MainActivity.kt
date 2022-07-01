c9390dc6f3d4a8259c311e6f810330e2b90818de
afonsoalves12 <afonsosilvalves@gmail.com>
2022-07-01 18:52:46 +0100

    Correção de erros

--- app/src/main/java/pt/ipg/app_entregas/MainActivity.kt
+++ app/src/main/java/pt/ipg/app_entregas/MainActivity.kt
@@ -1,7 +1,10 @@
 package pt.ipg.app_entregas
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -12,6 +15,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    var idMenuAtual = R.menu.menu_main
+        get() = field
+        set(value) {
+            if (value != field) {
+                field = value
+                invalidateOptionsMenu()
+            }
+        }
+
+    var menu: Menu? = null
+
+    var fragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +44,50 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(idMenuAtual, menu)
+        this.menu = menu
+        return true
+    }
+
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_activity_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        val opcaoProcessada: Boolean
+
+        if (fragment is MenuPrincipalFragment) {
+            opcaoProcessada = (fragment as MenuPrincipalFragment).processaOpcaoMenu(item)
+        } else if (fragment is ListaClienteFragment) {
+            opcaoProcessada = (fragment as ListaClienteFragment).processaOpcaoMenu(item)
+        } else if (fragment is EditarClienteFragment) {
+            opcaoProcessada = (fragment as EditarClienteFragment).processaOpcaoMenu(item)
+        }
+
+        else {
+            opcaoProcessada = false
+        }
+        if (opcaoProcessada) return true
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    fun mostraOpcoesAlterarEliminar(mostra: Boolean) {
+        menu!!.findItem(R.id.action_alterar).setVisible(mostra)
+        menu!!.findItem(R.id.action_guardar).setVisible(mostra)
+    }
+
+    fun atualizaTitulo(id_string_titulo: Int) {
+        binding.toolbar.setTitle(id_string_titulo)
     }
 }
